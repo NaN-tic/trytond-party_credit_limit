112816ecd4f3caa467d9e6ac3d52d5f20ca6eed3
?ngel ?lvarez <angel@nan-tic.com>
2014-07-07 10:41:32 +0200

    initial commit

---
+++ party.py
@@ -0,0 +1,193 @@
+#The COPYRIGHT file at the top level of this repository contains the full
+#copyright notices and license terms.
+from decimal import Decimal
+from sql import Literal
+from sql.aggregate import Sum
+from sql.conditionals import Coalesce, Case
+
+from trytond.model import fields
+from trytond.pool import Pool, PoolMeta
+from trytond.transaction import Transaction
+from trytond.wizard import Wizard, StateAction
+
+__all__ = ['Party', 'OpenLimit']
+__metaclass__ = PoolMeta
+
+
+class Party:
+    __name__ = 'party.party'
+
+    unpayed_amount = fields.Function(fields.Numeric('Unpayed amount'),
+        'get_accounting_amount')
+    pending_amount = fields.Function(fields.Numeric('Pending amount'),
+        'get_accounting_amount')
+    draft_invoices_amount = fields.Function(fields.Numeric(
+            'Draft invoices amount'),
+        'get_draft_invoices_amount')
+    uninvoiced_amount = fields.Function(fields.Numeric('Uninvoiced amount'),
+        'get_uninvoiced_amount')
+    amount_to_limit = fields.Function(fields.Numeric('Amount to limit'),
+        'get_amounts')
+    limit_percent = fields.Function(fields.Numeric('% Limit'),
+        'get_amounts')
+
+    @classmethod
+    def get_accounting_amount(cls, parties, names):
+        res = {}
+        pool = Pool()
+        MoveLine = pool.get('account.move.line')
+        Account = pool.get('account.account')
+        Date = pool.get('ir.date')
+        User = pool.get('res.user')
+
+        for name in names:
+            if name not in ('unpayed_amount', 'pending_amount'):
+                raise Exception('Bad argument')
+            res[name] = dict((p.id, Decimal('0.0')) for p in parties)
+
+        line = MoveLine.__table__()
+        account = Account.__table__()
+        today = Date.today()
+        transaction = Transaction()
+        cursor = transaction.cursor
+        line_query, _ = MoveLine.query_get(line)
+
+        user_id = transaction.user
+        if user_id == 0 and 'user' in Transaction().context:
+            user_id = transaction.context['user']
+        user = User(user_id)
+        if not user.company:
+            return res
+        company_id = user.company.id
+
+        amount = Coalesce(line.debit, 0) - Coalesce(line.credit, 0)
+        cursor.execute(*line.join(account,
+                condition=account.id == line.account
+            ).select(line.party,
+                Sum(Case((line.maturity_date < today, amount),
+                        else_=Literal(0))).as_('unpayed_amount'),
+                Sum(Case((line.maturity_date == None or
+                            line.maturity_date >= today, amount),
+                        else_=Literal(0))).as_('pending_amount'),
+            where=account.active
+            & (account.kind == 'receivable')
+            & (line.reconciliation == None)
+            & (account.company == company_id)
+            & line_query,
+            group_by=(line.party)))
+        for party_id, unpayed, pending in cursor.fetchall():
+            for name, value in (('unpayed_amount', unpayed),
+                    ('pending_amount', pending)):
+                if not name in names:
+                    continue
+                # SQLite uses float for SUM
+                if not isinstance(value, Decimal):
+                    value = Decimal(str(value))
+                res[name][party_id] = value
+        return res
+
+    @classmethod
+    def get_draft_invoices_amount(cls, parties, name):
+        pool = Pool()
+        Invoice = pool.get('account.invoice')
+        res = {}.fromkeys([p.id for p in parties], Decimal('0.0'))
+        without_sales = Transaction().context.get('without_sales', False)
+        domain = [
+            ('type', 'in', ('out_invoice', 'out_credit_note')),
+            ('party', 'in', [p.id for p in parties]),
+            ('state', 'in', ['draft', 'validated']),
+            ]
+        for invoice in Invoice.search(domain):
+            if without_sales and invoice.sales:
+                continue
+            amount = invoice.untaxed_amount
+            if 'credit_note' in invoice.type:
+                amount = amount.copy_negate()
+            res[invoice.party.id] += amount
+        return res
+
+    @classmethod
+    def get_uninvoiced_amount(cls, parties, name):
+        pool = Pool()
+        Sale = pool.get('sale.sale')
+        Currency = pool.get('currency.currency')
+        User = pool.get('res.user')
+
+        amounts = {}.fromkeys([p.id for p in parties], Decimal('0.0'))
+        sales = Sale.search([
+                ('party', 'in', [p.id for p in parties]),
+                ('state', '=', 'processing'),
+                ])
+
+        user = User(Transaction().user)
+        if not user.company:
+            return amounts
+        currency = user.company.currency
+        for sale in sales:
+            amount = 0
+            for line in sale.lines:
+                amount += Currency.compute(sale.currency, line.amount,
+                    currency, round=False)
+                for invoice_line in line.invoice_lines:
+                    invoice = invoice_line.invoice
+                    if invoice:
+                        amount -= Currency.compute(invoice.currency,
+                            invoice_line.amount, currency)
+            amounts[sale.party.id] += amount
+        return amounts
+
+    @classmethod
+    def get_amounts(cls, parties, names):
+        res = {}
+        for name in names:
+            if name not in ('amount_to_limit', 'limit_percent'):
+                raise Exception('Bad argument')
+            res[name] = {}.fromkeys([p.id for p in parties],
+                Decimal('100.0') if name == 'limit_percent'
+                else Decimal('0.0'))
+        for party in parties:
+            if 'amount_to_limit' in names:
+                limit_amount = party.credit_limit_amount or Decimal('0.0')
+                res['amount_to_limit'][party.id] = (
+                    limit_amount - party.credit_amount)
+            if party.credit_limit_amount and 'limit_percent' in names:
+                res['limit_percent'][party.id] = (Decimal('100.0') *
+                    party.credit_amount / party.credit_limit_amount)
+
+        for key in res.keys():
+            if key not in names:
+                del res[key]
+        return res
+
+    @classmethod
+    def get_credit_amount(cls, parties, name):
+        amounts = super(Party, cls).get_credit_amount(parties, name)
+        with Transaction().set_context(without_sales=True):
+            uninvoiced = cls.get_draft_invoices_amount(parties, name)
+            for party, value in uninvoiced.iteritems():
+                amounts[party] += value
+        return amounts
+
+    @classmethod
+    def _credit_limit_to_lock(cls):
+        models = super(Party, cls)._credit_limit_to_lock()
+        return models + ['account.invoice', 'sale.sale']
+
+
+class OpenLimit(Wizard):
+    'Open Credit Limit'
+    __name__ = 'party.party.open_credit_limit'
+    start_state = 'open_'
+    open_ = StateAction('party_credit_limit.act_party_credit_limit_form')
+
+    def do_open_(self, action):
+        active_ids = Transaction().context['active_ids']
+        data = {
+            'res_id': active_ids
+            }
+        if len(active_ids) == 1:
+            action['views'].reverse()
+        return action, data
+
+    def transition_open_(self):
+        return 'end'
---
+++ tests/test_party_credit_limit.py
@@ -0,0 +1,288 @@
+#!/usr/bin/env python
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
+from decimal import Decimal
+import unittest
+import datetime
+from dateutil.relativedelta import relativedelta
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
+from trytond.transaction import Transaction
+
+
+class TestCase(unittest.TestCase):
+    'Test module'
+
+    def setUp(self):
+        trytond.tests.test_tryton.install_module('party_credit_limit')
+        self.account = POOL.get('account.account')
+        self.invoice = POOL.get('account.invoice')
+        self.journal = POOL.get('account.journal')
+        self.field = POOL.get('ir.model.field')
+        self.move = POOL.get('account.move')
+        self.party = POOL.get('party.party')
+        self.payment_term = POOL.get('account.invoice.payment_term')
+        self.period = POOL.get('account.period')
+        self.property = POOL.get('ir.property')
+        self.product = POOL.get('product.product')
+        self.sale = POOL.get('sale.sale')
+        self.tax = POOL.get('account.tax')
+        self.tax_code = POOL.get('account.tax.code')
+        self.template = POOL.get('product.template')
+        self.uom = POOL.get('product.uom')
+
+    def test0005views(self):
+        'Test views'
+        test_view('party_credit_limit')
+
+    def test0006depends(self):
+        'Test depends'
+        test_depends()
+
+    def test0010check_credit_limit(self):
+        'Test check_credit_limit'
+        with Transaction().start(DB_NAME, USER, context=CONTEXT):
+            receivable, = self.account.search([
+                    ('kind', '=', 'receivable'),
+                    ])
+            revenue, = self.account.search([
+                    ('kind', '=', 'revenue'),
+                    ])
+            account_tax, = self.account.search([
+                    ('kind', '=', 'other'),
+                    ('name', '=', 'Main Tax'),
+                    ])
+            journal, = self.journal.search([], limit=1)
+            period, = self.period.search([], limit=1)
+            party, = self.party.create([{
+                        'name': 'Party',
+                        'addresses': [('create', [{}])],
+                        'account_receivable': receivable.id,
+                        }])
+
+            self.assertEqual(party.credit_amount, Decimal('0.0'))
+            self.assertEqual(party.unpayed_amount, Decimal('0.0'))
+            self.assertEqual(party.pending_amount, Decimal('0.0'))
+            self.assertEqual(party.draft_invoices_amount, Decimal('0.0'))
+            self.assertEqual(party.uninvoiced_amount, Decimal('0.0'))
+            self.assertEqual(party.limit_percent, Decimal('100.0'))
+            self.assertEqual(party.amount_to_limit, Decimal('0.0'))
+
+            party.credit_limit_amount = Decimal('1000.0')
+            party.save()
+
+            self.assertEqual(party.credit_amount, Decimal('0.0'))
+            self.assertEqual(party.unpayed_amount, Decimal('0.0'))
+            self.assertEqual(party.pending_amount, Decimal('0.0'))
+            self.assertEqual(party.draft_invoices_amount, Decimal('0.0'))
+            self.assertEqual(party.uninvoiced_amount, Decimal('0.0'))
+            self.assertEqual(party.limit_percent, Decimal('0.0'))
+            self.assertEqual(party.amount_to_limit, Decimal('1000.0'))
+
+            party = self.party(party.id)
+
+            term, = self.payment_term.create([{
+                        'name': 'Payment term',
+                        'lines': [
+                            ('create', [{
+                                        'sequence': 0,
+                                        'type': 'remainder',
+                                        'days': 0,
+                                        'months': 0,
+                                        'weeks': 0,
+                                        }])]
+                        }])
+
+            tx = self.tax_code.create([{
+                            'name': 'invoice base',
+                            },
+                        {
+                            'name': 'invoice tax',
+                            },
+                        {
+                            'name': 'credit note base',
+                            },
+                        {
+                            'name': 'credit note tax',
+                        }])
+            invoice_base, invoice_tax, credit_note_base, credit_note_tax = tx
+            tax, = self.tax.create([{
+                        'name': 'Tax 1',
+                        'description': 'Tax 1',
+                        'type': 'percentage',
+                        'rate': Decimal('.10'),
+                        'invoice_account': account_tax.id,
+                        'credit_note_account': account_tax.id,
+                        'invoice_base_code': invoice_base.id,
+                        'invoice_tax_code': invoice_tax.id,
+                        'credit_note_base_code': credit_note_base.id,
+                        'credit_note_tax_code': credit_note_tax.id,
+                        }])
+            field, = self.field.search([
+                    ('name', '=', 'account_revenue'),
+                    ('model.model', '=', 'product.template'),
+                    ])
+
+            self.property.create([{
+                        'value': 'account.account,%d' % revenue.id,
+                        'field': field.id,
+                        'res': None,
+                        }])
+            today = datetime.date.today()
+            yesterday = today - relativedelta(days=1)
+            self.move.create([{
+                        'journal': journal.id,
+                        'period': period.id,
+                        'date': period.start_date,
+                        'lines': [
+                            ('create', [{
+                                        'debit': Decimal(100),
+                                        'account': receivable.id,
+                                        'party': party.id,
+                                        'maturity_date': yesterday,
+                                        }, {
+                                        'credit': Decimal(100),
+                                        'account': revenue.id,
+                                        'party': party.id,
+                                        }]),
+                            ],
+                        }, {
+                        'journal': journal.id,
+                        'period': period.id,
+                        'date': period.start_date,
+                        'lines': [
+                            ('create', [{
+                                        'debit': Decimal(200),
+                                        'account': receivable.id,
+                                        'party': party.id,
+                                        }, {
+                                        'credit': Decimal(200),
+                                        'account': revenue.id,
+                                        'party': party.id,
+                                        }]),
+                            ],
+                        }])
+
+            kg, = self.uom.search([('name', '=', 'Kilogram')])
+            template, = self.template.create([{
+                        'name': 'Test credit limit',
+                        'type': 'goods',
+                        'list_price': Decimal(20),
+                        'cost_price': Decimal(10),
+                        'cost_price_method': 'fixed',
+                        'default_uom': kg.id,
+                        'salable': True,
+                        'sale_uom': kg.id,
+                        'delivery_time': 0,
+                        }])
+            product, = self.product.create([{
+                        'template': template.id,
+                        }])
+            sales = self.sale.create([{
+                        'party': party.id,
+                        'shipment_address': party.addresses[0].id,
+                        'invoice_address': party.addresses[0].id,
+                        'invoice_method': 'shipment',
+                        'payment_term': term.id,
+                        'lines': [
+                            ('create', [{
+                                        'type': 'line',
+                                        'sequence': 0,
+                                        'product': product.id,
+                                        'description': 'invoice_line',
+                                        'unit': kg.id,
+                                        'quantity': 1,
+                                        'unit_price': Decimal('200.0'),
+                                        'taxes': [
+                                            ('add', [tax.id])],
+                                        }])],
+                        }, {
+                        'party': party.id,
+                        'shipment_address': party.addresses[0].id,
+                        'invoice_address': party.addresses[0].id,
+                        'payment_term': term.id,
+                        'lines': [
+                            ('create', [{
+                                        'type': 'line',
+                                        'sequence': 0,
+                                        'description': 'invoice_line',
+                                        'quantity': 1,
+                                        'unit_price': Decimal('50.0'),
+                                        'taxes': [
+                                            ('add', [tax.id])],
+                                        }])],
+                        }])
+
+            self.sale.quote(sales)
+            self.sale.confirm(sales)
+            self.sale.process(sales)
+
+            party = self.party(party.id)
+
+            self.assertEqual(party.unpayed_amount, Decimal('100.0'))
+            self.assertEqual(party.pending_amount, Decimal('200.0'))
+            self.assertEqual(party.draft_invoices_amount,
+                Decimal('50.0'))
+            self.assertEqual(party.uninvoiced_amount, Decimal('200.0'))
+            self.assertEqual(party.credit_amount, Decimal('550.0'))
+            self.assertEqual(party.amount_to_limit, Decimal('450.0'))
+            self.assertEqual(party.limit_percent, Decimal('55.0'))
+            party.credit_limit_amount = Decimal('0.0')
+            party.save()
+
+            party = self.party(party.id)
+            self.assertEqual(party.limit_percent, Decimal('100.0'))
+            self.assertEqual(party.amount_to_limit,
+                party.credit_amount.copy_negate())
+
+            party.credit_limit_amount = Decimal('500.0')
+            party.save()
+
+            party = self.party(party.id)
+            self.assertEqual(party.limit_percent, Decimal('110.0'))
+            self.assertEqual(party.amount_to_limit, Decimal('-50.0'))
+
+            #Create an invoice and test it get's added to draft invoices amount
+            #and in credit_amount but not in uninvoiced_amount
+            invoice, = self.invoice.create([{
+                        'number': '1',
+                        'invoice_date': period.start_date,
+                        'type': 'out_invoice',
+                        'party': party.id,
+                        'invoice_address': party.addresses[0].id,
+                        'journal': journal.id,
+                        'account': receivable.id,
+                        'payment_term': term.id,
+                        'lines': [
+                            ('create', [{
+                                        'invoice_type': 'out_invoice',
+                                        'type': 'line',
+                                        'sequence': 0,
+                                        'description': 'invoice_line',
+                                        'account': revenue.id,
+                                        'quantity': 1,
+                                        'unit_price': Decimal('50.0'),
+                                        'taxes': [
+                                            ('add', [tax.id])],
+                                        }])],
+                        }])
+
+            party = self.party(party.id)
+            self.assertEqual(party.unpayed_amount, Decimal('100.0'))
+            self.assertEqual(party.pending_amount, Decimal('200.0'))
+            self.assertEqual(party.draft_invoices_amount, Decimal('100.0'))
+            self.assertEqual(party.uninvoiced_amount, Decimal('200.0'))
+            self.assertEqual(party.credit_amount, Decimal('600.0'))
+
+
+def suite():
+    suite = trytond.tests.test_tryton.suite()
+    from trytond.modules.account.tests import test_account
+    for test in test_account.suite():
+        #Skip doctest
+        class_name = test.__class__.__name__
+        if test not in suite and class_name != 'DocFileCase':
+            suite.addTest(test)
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
+    return suite
